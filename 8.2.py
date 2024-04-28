02a6e689e38720420882564f55537f3b45af1e63
Kamil Toporek <kamil.toporek98@gmail.com>
2024-04-28 22:12:42 +0200

    youngest, earlier

--- 8.2.py
+++ 8.2.py
@@ -18,6 +18,10 @@ najczestszy_rok = liczba_albumow_na_rok.idxmax()
 albumy_1960_1990 = data[0][(data[0]['Rok'] >= 1960) & (data[0]['Rok'] <= 1990)]
 liczba_albumow_1960_1990 = len(albumy_1960_1990)
 
+najmlodszy_album_rok = data[0]['Rok'].max()
+
+najwczeniej_wydane_albumy = data[0].groupby('Artysta').agg(Rok=('Rok', 'min')).reset_index()
+
 
 
 
@@ -27,3 +31,6 @@ print("\nLiczba pojedynczych artystów na liście:", liczba_artystow)
 print("\nNajczęściej występujące zespoły na liście:", najczestsze_zespoly.head(10))
 print("\nRok z największą liczbą albumów na liście:", najczestszy_rok)
 print("\nLiczba albumów wydanych między 1960 a 1990 rokiem włącznie:", liczba_albumow_1960_1990)
+print("\nNajmłodszy album na liście został wydany w roku:", najmlodszy_album_rok)
+print("\nLista najwcześniej wydanych albumów dla każdego artysty:")
+print(najwczeniej_wydane_albumy)
